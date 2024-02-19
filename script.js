081e56f8431b14c4ebc58999c57ce174bdcf4314
mamakdude <95206752+mamakdude@users.noreply.github.com>
2024-02-19 16:20:35 +0800

    Add files via upload

---
+++ script.js
@@ -0,0 +1,33 @@
+const noButton = document.getElementById("no-button");
+const title = document.getElementById('cute-font');
+const mainImage = document.getElementById('image');
+const yesButton = document.getElementById("yes-button");
+
+// Add a mouseover event listener to move the "No" button randomly
+noButton.addEventListener("mouseover", () => {
+  const x = Math.floor(Math.random() * window.innerWidth);
+  const y = Math.floor(Math.random() * window.innerHeight);
+
+  // Restrict the "No" button's movement within the bounds of the screen
+  const buttonWidth = noButton.offsetWidth;
+  const buttonHeight = noButton.offsetHeight;
+  const maxX = window.innerWidth - buttonWidth;
+  const maxY = window.innerHeight - buttonHeight;
+  const adjustedX = x < maxX ? x : maxX;
+  const adjustedY = y < maxY ? y : maxY;
+
+  // Apply the new position to the button
+  noButton.style.position = "absolute";
+  noButton.style.left = `${adjustedX}px`;
+  noButton.style.top = `${adjustedY}px`;
+});
+
+
+yesButton.addEventListener("click",function(){
+    title.textContent= "I love you sayang*";
+    mainImage.src= "./assets/imges/loveyoubear.gif";
+    yesButton.style.display='none';
+    noButton.style.display='none';
+})
+
+  
